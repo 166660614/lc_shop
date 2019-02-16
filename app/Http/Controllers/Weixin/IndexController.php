0df187e4e12ae276a26e6f69798ceb9deb267f65
166660614 <166660614@qq.com>
2019-02-16 17:02:34 +0800

    ad

--- app/Http/Controllers/Weixin/IndexController.php
+++ app/Http/Controllers/Weixin/IndexController.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 
 class IndexController extends Controller
 {
-    public function wxEvent(){
+    public function wxToken(){
         $data=file_put_contents("php://input");
         $log_str=date('Y-m-d H:i:s')."\n".$data."\n<<<<<<<";
         file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
