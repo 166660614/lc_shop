49740b4f493bf90f0539509376b453e0dadad248
166660614 <166660614@qq.com>
2019-02-17 14:26:09 +0800

    获取微信用户信息

--- app/Http/Controllers/Weixin/IndexController.php
+++ app/Http/Controllers/Weixin/IndexController.php
@@ -14,14 +14,30 @@ class IndexController extends Controller
         $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
         file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
     }
-    public function validToken(){
-        $data=file_put_contents("php://input");
-        $log_str=date('Y-m-d H:i:s')."\n".$data."\n<<<<<<<";
-        file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
-    }
     public function wxEvent()
     {
         echo $_GET['echostr'];
     }
     //获取AccessToken
+    public function getAccessToken(){
+        $token=Redis::get($this->redis_weixin_access_token);
+        if(!$token){
+            $url="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".env('WEIXIN_APPID')."&secret=".env('WEIXIN_APPSECRET');
+            $data=json_decode(file_get_contents($url),true);
+            //记录缓存
+            $token=$data['access_token'];
+            Redis::set($this->redis_weixin_access_token);
+            Redis::setTimeout($this->redis_weixin_access_token,3600);
+        }
+        return $token;
+    }
+    // 获取用户信息
+    public function getUserInfo()
+    {
+        $openid=$_GET['openid'];
+        $access_token = $this->getAccessToken();
+        $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+        $data = json_decode(file_get_contents($url),true);
+        echo '<pre>';print_r($data);echo '</pre>';
+    }
 }
