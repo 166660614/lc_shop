34ef7a693fd4027093a35525dd14fea2fae97658
166660614 <166660614@qq.com>
2019-02-17 13:17:35 +0800

    ds

--- app/Http/Controllers/Weixin/IndexController.php
+++ app/Http/Controllers/Weixin/IndexController.php
@@ -19,7 +19,7 @@ class IndexController extends Controller
         $log_str=date('Y-m-d H:i:s')."\n".$data."\n<<<<<<<";
         file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
     }
-    public function vxEvent()
+    public function wxEvent()
     {
         echo $_GET['echostr'];
     }
