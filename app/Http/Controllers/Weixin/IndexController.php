ab31dda10f512bd3ee3cefebefb58147ed26f047
166660614 <166660614@qq.com>
2019-02-17 12:40:40 +0800

    o

--- app/Http/Controllers/Weixin/IndexController.php
+++ app/Http/Controllers/Weixin/IndexController.php
@@ -7,10 +7,12 @@ use App\Http\Controllers\Controller;
 
 class IndexController extends Controller
 {
+    protected $redis_weixin_access_token='str:weixin_access_token';//微信 access_token
+    //接受事件推送
     public function wxEvent(){
         $data=file_put_contents("php://input");
         $log_str=date('Y-m-d H:i:s')."\n".$data."\n<<<<<<<";
         file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
     }
-    //获取扫描人信息
+    //获取AccessToken
 }
