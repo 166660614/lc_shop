65faa7b6bb75939a6105ba96b9fa4b0880f81839
166660614 <166660614@qq.com>
2019-02-18 11:01:11 +0800

    sa

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,5 @@ Route::post('/upload/pdf','Upload\TestUpload@uploadpdf');
 Route::get('/move/seat','Move\IndexController@index');
 Route::post('/move/order','Move\IndexController@order');
 //微信公众号
-Route::get('/weixin/valid','Weixin\IndexController@validToken');
-Route::post('/weixin/valid','Weixin\IndexController@validToken');
-Route::get('/weixin/valid1','Weixin\IndexController@wxEvent');//接受微信服务器事件推送
-Route::post('/weixin/valid1','Weixin\IndexController@validToken1');
+Route::post('/weixin/valid1','Weixin\IndexController@wxEvent');//接受微信服务器事件推送
+Route::get('/weixin/valid1','Weixin\IndexController@validToken1');
