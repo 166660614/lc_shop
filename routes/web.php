43144ea967aeb71b38f3114bacfe4a6e5ce7f207
166660614 <166660614@qq.com>
2019-02-20 22:21:47 +0800

    das

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/move/seat','Move\IndexController@index');
 Route::post('/move/order','Move\IndexController@order');
 //微信公众号
 Route::get('/weixin/token','Weixin\IndexController@getAccessToken');
-Route::get('/weixin/valid1','Weixin\IndexController@validToken1');
+Route::get('/weixin/valid1?echostr=aaa','Weixin\IndexController@validToken1');
 Route::post('/weixin/valid1','Weixin\IndexController@wxEvent');//接受微信服务器事件推送
 Route::get('/weixin/create_menus','Weixin\IndexController@zdyMenus');//创建自定义菜单
 Route::get('/weixin/del_menus','Weixin\IndexController@delMenus');//删除自定义菜单
