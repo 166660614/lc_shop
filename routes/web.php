fa4da0d3b74eb7c4c55d14804018308c316a16da
166660614 <166660614@qq.com>
2019-02-20 11:56:20 +0800

    fsdfd

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,4 @@ Route::get('/weixin/valid1','Weixin\IndexController@validToken1');
 Route::post('/weixin/valid1','Weixin\IndexController@wxEvent');//接受微信服务器事件推送
 Route::get('/weixin/create_menus','Weixin\IndexController@zdyMenus');//创建自定义菜单
 Route::get('/weixin/del_menus','Weixin\IndexController@delMenus');//删除自定义菜单
+Route::get('/weixin/send_all_content','Weixin\IndexController@sendTextAll');//群发成功
