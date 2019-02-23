37d35243227e6514f0115a8977f12c740e4586ee
166660614 <166660614@qq.com>
2019-02-23 08:52:49 +0800

    dasda

--- resources/views/admin/chat.blade.php
+++ resources/views/admin/chat.blade.php
@@ -2,9 +2,13 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <form>
     <table>
+        <tr>
+            <td>聊天记录</td>
+            <td><textarea name="" id="content" cols="30" rows="10"></textarea></td>
+        </tr>
         <input type="hidden" class="user_id" value="{{$user_id}}">
         <tr>
-            <td>文本消息：</td>
+            <td>发送内容：</td>
             <td><input type="text" id="news"></td>
         </tr>
         <tr>
@@ -19,6 +23,7 @@
             _this=$(this);
             var news=$('#news').val();
             var user_id=$('.user_id').val();
+            $('#content').text(news);
             $.ajax({
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
