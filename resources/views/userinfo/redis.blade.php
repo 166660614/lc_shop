13c8977779320371af381e46e8b6d6b6916ee429
166660614 <166660614@qq.com>
2019-03-08 11:32:38 +0800

    fdss

--- resources/views/userinfo/redis.blade.php
+++ resources/views/userinfo/redis.blade.php
@@ -1,16 +1,15 @@
 <table>
-{{$userinfo}}
-</table>
-<table>
     <tr>
         <td>用户id</td>
         <td>用户昵称</td>
         <td>关注时间</td>
     </tr>
+    @foreach($userinfo as $v)
     <tr>
-        <td></td>
+        <td>{{$userinfo['nickname']}}</td>
         <td></td>
         <td></td>
         <td></td>
     </tr>
+    @endforeach
 </table>
