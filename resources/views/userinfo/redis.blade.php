2ca93bb83f1c1c8c3af8101e8d16ce37851b16fc
166660614 <166660614@qq.com>
2019-03-08 11:48:40 +0800

    fdss

--- resources/views/userinfo/redis.blade.php
+++ resources/views/userinfo/redis.blade.php
@@ -1,12 +1,19 @@
-<table>
+<table border="1">
     <tr>
         <td>oppenid</td>
         <td>用户昵称</td>
         <td>性别</td>
     </tr>
     <tr>
+        <td><input type="checkbox"></td>
         <td>{{$userinfo['openid']}}</td>
         <td>{{$userinfo['nickname']}}</td>
         <td>{{$userinfo['sex']}}</td>
     </tr>
 </table>
+<script src="js/jquery-1.12.4.min.js"></script>
+<script>
+    $(function () {
+        alert(1)
+    })
+</script>
